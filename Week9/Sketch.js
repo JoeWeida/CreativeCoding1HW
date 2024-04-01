73bedfb45cfc7779e0ec2a3b8cb51ac11b44cdbe
JoeWeida <jw193732@umconnect.umt.edu>
2024-03-31 19:27:04 -0600

    made sketch

---
+++ Week9/Sketch.js
@@ -0,0 +1,17 @@
+function setup()
+{
+    createCanvas(300,500);
+}
+function draw() {
+    background(100);
+    ellipse(150,250,300,400);
+    textSize(35);
+    text('Whats up Brother!', 10, 30);
+    text('Joe Weida', 80, 490);
+    rect(100,330,100,40);
+    circle(100,200,65);
+    circle(200,200,65);
+    triangle(150,290, 100, 290, 150, 210);
+    line(100, 350, 200, 350);
+
+  }
